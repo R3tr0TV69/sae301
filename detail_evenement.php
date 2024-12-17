c2798ae5d6b47c7c74c7a79a1772f3b16396a067
alexisbertrand689 <alexiis.bertrand@gmail.com>
2024-12-17 15:48:04 +0100

    correction css

--- detail_evenement.php
+++ detail_evenement.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'config/config.php';
 
+$site = $pdo->query("SELECT * FROM gestion_site LIMIT 1")->fetch(PDO::FETCH_ASSOC);
+
 if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
     die('ID invalide');
 }
