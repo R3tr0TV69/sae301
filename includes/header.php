c4b6bed834149f274067ab4c04ef42618517d8c8
alexisbertrand689 <alexiis.bertrand@gmail.com>
2024-12-18 09:13:11 +0100

    responsive header landing page

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,26 @@
 <header>
     <a href="index.php"><img src="<?= htmlspecialchars($site['logo']) ?>" alt="Logo USV" width="200px"></a>
-    <ul>
+    <ul class="nav-links">
         <li><a href="index.php#statistiques">Stats</a></li>
         <li><a href="index.php#a-propos">À propos</a></li>
         <li><a href="index.php#evenements">Évènements</a></li>
         <li><a href="index.php#inscription">Inscription</a></li>
         <li><a href="index.php#avis">Avis</a></li>
     </ul>
-    <a href="login.php"><img src="images/icon.png" alt="Se connecter" width="120px" height="120px" id="profil-icon"></a>
+    <div class="burger-menu">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+    <div class="menu-mobile">
+        <ul>
+            <li><a href="index.php#statistiques">Stats</a></li>
+            <li><a href="index.php#a-propos">À propos</a></li>
+            <li><a href="index.php#evenements">Évènements</a></li>
+            <li><a href="index.php#inscription">Inscription</a></li>
+            <li><a href="index.php#avis">Avis</a></li>
+            <li><a href="login.php">Se connecter</a></li>
+        </ul>
+    </div>
+    <a class="connexion-link" href="login.php"><img src="images/icon.png" alt="Se connecter" width="120px" height="120px" id="profil-icon"></a>
 </header>
